291c4ef42a270ed29342e45c851f90f1263f7bfe
Johan Karl Larsen <johan@itlarsen.net>
2017-10-31 14:06:50 +0100

    before remove scp

--- 07 - Docker Swarm/ExampleApp/certs/generate.sh
+++ 07 - Docker Swarm/ExampleApp/certs/generate.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 set -x
-#openssl genrsa -out ca/myswarmrootCA.key 2048
-#openssl req -x509 -new -nodes -key ca/myswarmrootCA.key -days 3651 -out ca/myswarmrootCA.crt -subj '//O=Myswarm docker Sample\CN=Myswarm Root CA'
-#openssl genrsa -out host/myswarmregistry.key 2048
-#openssl req -new -key host/myswarmregistry.key -out myswarmregistry.csr -subj '//O=Myswarm docker sample\CN=myswarmregistry'
-#openssl x509 -req -in myswarmregistry.csr -CA ca/myswarmrootCA.crt -CAkey ca/myswarmrootCA.key -CAcreateserial -out host/myswarmregistry.crt -days 3650
-#rm -f myswarmregistry.csr
-openssl req -newkey rsa:4096 -nodes -sha256 -keyout host/myswarmregistry.key -x509 -days 365 -out host/myswarmregistry.crt -subj '//O=Myswarm docker sample\CN=myswarmregistry'
-exec bash # dont close window when done
+openssl req -newkey rsa:4096 -nodes -sha256 -keyout host/myswarmregistry.key -x509 -days 3650 -out host/myswarmregistry.crt -subj '//O=Myswarm docker sample\CN=myswarmregistry'
--- 07 - Docker Swarm/ExampleApp/manager-setup.ps1
+++ 07 - Docker Swarm/ExampleApp/manager-setup.ps1
@@ -4,12 +4,11 @@ $managerip = docker-machine ip manager
 
 # copy haproxy.cfg to manager
 & $gitbash -c "docker-machine scp './haproxy.cfg' manager:~ && echo 'OK' || exec bash"
+Start-Sleep -Seconds 3
 
 # copy registry cert files to manager
-& $gitbash -c "docker-machine scp -r ./certs/host/ manager:~/certs && echo 'OK' || exec bash"
-
-# copy certs we need on manager and worker nodes to get docker pull/push work for loacal registry
-docker-machine ssh manager "sudo mkdir -p /etc/docker/certs.d/myswarmregistry:5000;sudo cp /home/docker/certs/myswarmregistry.crt /etc/docker/certs.d/myswarmregistry:5000/ca.crt"
+& $gitbash -c "docker-machine scp -r ./certs/host/ manager:~/certs && docker-machine ssh manager 'sudo mkdir -p /etc/docker/certs.d/myswarmregistry:5000 && sudo cp /home/docker/certs/myswarmregistry.crt /etc/docker/certs.d/myswarmregistry:5000/ca.crt'"
+Start-Sleep -Seconds 3
 
 # Redirect all docker commands to manager VM
 docker-machine env manager | Invoke-Expression
@@ -26,7 +25,7 @@ $myswarmregistryca = [IO.File]::ReadAllText("$pwd\certs\host\myswarmregistry.crt
 foreach ($node in $workernodes)
 {
 	docker node update --label-add type=mvc $node
-	# make sure worker node trust our registry, and find it exposed on manager:5000 
+	# make sure worker node trust our registry, and can find it exposed on manager:5000 
 	docker-machine ssh $node "sudo mkdir -p /etc/docker/certs.d/myswarmregistry:5000; \
 								printf '%s' '$myswarmregistryca' | sudo tee /etc/docker/certs.d/myswarmregistry:5000/ca.crt; \
 								echo $managerip  myswarmregistry | sudo tee -a /etc/hosts" 
@@ -35,7 +34,7 @@ foreach ($node in $workernodes)
 docker network create -d overlay swarm_backend
 
 echo "======> Setting up mysql service. Could take some time ..."
-docker -D service create --name mysql --mount type=volume,source=productdata,destination=/var/lib/mysql --constraint "node.hostname == dbhost" --replicas 1 --network swarm_backend -e MYSQL_ROOT_PASSWORD=mysecret -e bindaddress=0.0.0.0 --detach=false mysql:8.0.0
+docker service create --name mysql --mount type=volume,source=productdata,destination=/var/lib/mysql --constraint "node.hostname == dbhost" --replicas 1 --network swarm_backend -e MYSQL_ROOT_PASSWORD=mysecret -e bindaddress=0.0.0.0 --detach=false mysql:8.0.0
 
 echo "======> Initializing database on dbhost:3306"
 docker service update --detach=false --publish-add 3306:3306 mysql
@@ -55,6 +54,7 @@ docker push $exampleappimagename
 docker -D service create --detach=false --name mvcapp --constraint "node.labels.type==mvc" --replicas 5 --network swarm_backend -p 3000:80 -e DBHOST=mysql $exampleappimagename
 
 docker container run -d --name loadbalancer -v "/home/docker/haproxy.cfg:/usr/local/etc/haproxy/haproxy.cfg" --add-host manager:$managerip -p 80:80 haproxy:1.7.0
+# display any errors
 docker logs loadbalancer
 
 docker-machine env -u | Invoke-Expression
